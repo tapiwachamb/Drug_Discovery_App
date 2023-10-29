aafc06cce00f9e702bb97f72aabdbabdb47c639d
tapiwa chamboko <80702706+tapiwachamb@users.noreply.github.com>
2023-10-29 11:20:56 +0200

    Add files via upload

--- apps/home.py
+++ apps/home.py
@@ -27,29 +27,5 @@ def app():
     st.write('')
 
     st.write('**Welcome to the Drug Discovery App**')
-   # st.image('3.png', width=800)
-    def load_lottieurl(url: str):
-        r = requests.get(url)
-        if r.status_code != 200:
-            return None
-        return r.json()
-    # "https://assets1.lottiefiles.com/packages/lf20_z3wd7moi.json"
-    #def load_lottiefile(filepath: str):
-        #with open(filepath, "r") as f:
-            #return json.load(f)
-    #lottie_coding = load_lottiefile("lottiefile.json")  # replace link to local lottie file
-    lottie_url = "https://assets2.lottiefiles.com/packages/lf20_t7ok1nhk.json"
-    lottie_json = load_lottieurl(lottie_url)
-
-    st_lottie(
-    lottie_json,
-    speed=1,
-    reverse=False,
-    loop=True,
-    quality="low", # medium ; high
-    renderer="svg", # canvas
-    height=None,
-    width=None,
-    key=None,
-)
+    st.image('12.png', width=800)
   
